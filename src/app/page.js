3c6676609730e74a719b3bf05db52bed7c8663f1
rafi-aman3 <rafiaman03@gmail.com>
2025-11-14 21:31:12 +0600

    necessary packages installed

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,11 @@
+"use client"
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       MOVIE WATCHLIST COMING SOON
+      <Button>Hurrah!</Button>
     </div>
   );
 }
